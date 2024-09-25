4de815244eb27e24f63b69141d27a7d3c81c7938
Md. Asadul Islam <01913404212.asad@gmail.com>
2024-09-25 08:44:52 +0600

    Todo app Task Management

---
+++ todo_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    path('', views.task_list, name = "task_list"),
+    path('<int:pk>/', views.task_details, name = "task_details"),
+]
